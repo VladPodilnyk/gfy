15ced63ea36efdfdf7709cc340040b7c94bac1e2
vladpodilnyk <vlad.podilnyk@gmail.com>
2025-11-21 16:55:00 +0400

    playing with api.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,34 @@
+use std::error::Error;
+
 use image::DynamicImage;
-use std::io::Error;
 
 pub struct Converter {
-    image: DynamicImage,
+    original: DynamicImage,
+    transformed: Option<DynamicImage>,
 }
 
 impl Converter {
-    pub fn load_image(file_name: &str) -> Result<Self, Error> {
-        unimplemented!()
+    pub fn load_image(file_name: &str) -> Result<Self, Box<dyn Error>> {
+        let image = image::open(file_name)?;
+        return Ok(Converter {
+            original: image,
+            transformed: None,
+        });
     }
 
-    pub fn convert_to_ascii(&self) -> Result<Self, Error> {
-        unimplemented!()
+    pub fn convert_to_ascii(&mut self) -> Result<&mut Self, Box<dyn Error>> {
+        self.transformed = Some(self.original.grayscale());
+        Ok(self)
     }
 
-    pub fn write_file(&self, file_name: &str) -> Result<(), Error> {
-        unimplemented!()
+    pub fn write_file(&self, file_name: &str) -> Result<(), Box<dyn Error>> {
+        match &self.transformed {
+            None => Err("No transformed image".into()),
+            Some(img) => img.save(file_name).map_err(|e| e.into()),
+        }
     }
 
-    pub fn print(&self) -> Result<(), Error> {
+    pub fn print(&self) -> Result<(), Box<dyn Error>> {
         unimplemented!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 use clap::Parser;
 use gfy::Converter;
-use std::io::Error;
+use std::error::Error;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about=None)]
 struct Args {
-    #[arg(short)]
-    file_name: String,
+    #[arg(short = 'f')]
+    input_file: String,
+
+    #[arg(short = 'o')]
+    output_file: String,
 }
 
 fn main() {
     let args = Args::parse();
-    run(&args.file_name).unwrap_or_else(|err| eprintln!("Failed to convert image to ascii: {err}"))
+    run(&args).unwrap_or_else(|err| eprintln!("Failed to convert image to ascii: {err}"))
 }
 
-fn run(file_name: &str) -> Result<(), Error> {
-    Converter::load_image(file_name)?
+fn run(args: &Args) -> Result<(), Box<dyn Error>> {
+    Converter::load_image(&args.input_file)?
         .convert_to_ascii()?
-        .print()
+        .write_file(&args.output_file)
 }
